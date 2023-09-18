7256fdd03330f61ea945d59fca2a471f83ea9f1d
Mohammad Ashif <mohammadashif510@gmail.com>
2023-09-19 00:36:22 +0600

    btn toggle done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "../Link/Link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+
+
+    const [open, setOpen] = useState(false);
+
     const routerLinks = [
         { id: 1, name: "Home", path: "/" },
         { id: 2, name: "About", path: "/about" },
@@ -12,7 +17,13 @@ const Navbar = () => {
 
     return (
         <div>
-            <ul className="md:flex  gap-4 text-2xl">
+            <div className="text-2xl md:hidden" onClick={()=>setOpen(!open)}>
+                {
+                    open === true ? <AiOutlineMenu/> : <AiOutlineClose/>
+                }
+                
+            </div>
+            <ul className="md:flex  gap-4 text-2xl ">
                 {routerLinks.map((link) => (
                     <Link key={link.id} link={link}></Link>
                 ))}
